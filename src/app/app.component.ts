1830f99053e4ae0d357d84ba2b19f3813edb85f9
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-10-05 15:23:15 +0200

    Added retrieving form data and resetting form
    Added retrieving form data and resetting form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion: String = 'pet';
   answer: String = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,6 +45,15 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
